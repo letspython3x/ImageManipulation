27d5a8da743cf221ca1b75e5848722e59f0e08ad
Abhishake <letspython3.x@gmail.com>
2022-04-14 07:56:43 +0530

    Update README and input-output

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# play_with_pillow
-Here are some scripts to play with Pillow (PIL) Python Imaging Library
+# Image Manipulation via pillow (PIL)
+
+Here are sample programs for image manipulation using `pillow` (PIL - Python Imaging Library)
+
+### Sample Invocation
+
+
+> python app/main.py -ip input/rose1_example.jpg -op output/rose_flipped.jpg -ro 90 -rsh 100 -rsw 200
+
+### Input Image 1
+
+![image info](./input/parrot_example.jpeg)
+
+### Output Image 1
+
+![image info](./output/parrot_flipped.jpg)
+
+
+### Input Image 2
+
+![image info](./input/rose1_example.jpg)
+
+### Output Image 1
+
+![image info](./output/rose_flipped.jpg)
--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ import argparse
 from PIL import Image
 
 
-def flip_resize_image(imagePath:str, outputFileImage:str, 
-                      rotateAngle:int, resizeHeight:int=None,
-                      resizeWidth:int=None):
+def flip_resize_image(imagePath: str, outputFileImage: str,
+                      rotateAngle: int, resizeHeight: int = None,
+                      resizeWidth: int = None):
     with Image.open(imagePath) as im:
         newImage = im.rotate(rotateAngle)
         if resizeHeight and resizeWidth:
@@ -16,14 +16,29 @@ def flip_resize_image(imagePath:str, outputFileImage:str,
         print(f"*** image saved at {outputFileImage}")
 
 
+def verify_image_extension(imagePath: str):
+    _, imageExt = os.path.splitext(imagePath)
+    if imageExt not in (".jpg", ".jpeg", ".png"):
+        raise RuntimeError("Kindly provide a valid image.")
+
+
+def get_output_file_name(ipImage: str):
+    imageName, imageExt = os.path.splitext(ipImage)
+    return f"{imageName}_updated.jpg"
+
+
 def parse_cmdline():
-    parser = argparse.ArgumentParser(description="A command line utility for manipulating images.")
-    parser.add_argument("-ip", "--input_image", dest="ip_image", 
+    parser = argparse.ArgumentParser(
+        description="A command line utility for"
+                    "manipulating images.")
+    parser.add_argument("-ip", "--input_image", dest="ip_image",
                         help="Absolute path to the input image.")
-    parser.add_argument("-op", "--output_image", dest="op_image", 
-                        default=None, help="Path to the output image.")
-    parser.add_argument("-ro", "--rotate", dest="rotate", 
-                        type=int, default=360, help="Rotate image by an angle.")
+    parser.add_argument("-op", "--output_image", dest="op_image",
+                        default=None,
+                        help="Path to the output image.")
+    parser.add_argument("-ro", "--rotate", dest="rotate",
+                        type=int, default=360,
+                        help="Rotate image by an angle.")
     parser.add_argument("-rsh", "--resize_height", dest="rs_height",
                         type=int, help="Resize image Height")
     parser.add_argument("-rsw", "--resize_width", dest="rs_width", 
@@ -33,22 +48,13 @@ def parse_cmdline():
     return args
 
 
-def verify_image_extension(imagePath:str):
-    _, imageExt = os.path.splitext(imagePath)
-    
-    if imageExt not in ("jpg", "jpeg", "png"):
-        raise RuntimeError("Kindly provide a valid image.")
-
-def get_output_file_name(ipImage):
-    imageName, imageExt = os.path.splitext(ipImage)
-    return f"{imageName}_updated.jpg"
-
 def main():
     args = parse_cmdline()
     verify_image_extension(args.ip_image)
     opImagePath = args.op_image or get_output_file_name(args.ip_image)
-    flip_resize_image(args.ip_image, opImagePath, args.rotate, args.rs_height, args.rs_width)
-    
+    flip_resize_image(args.ip_image, opImagePath, args.rotate,
+                      args.rs_height, args.rs_width)
+
 
 if __name__ == "__main__":
     main()
